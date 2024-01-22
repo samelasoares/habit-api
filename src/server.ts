5a18f76f47eebf4f0bed39f97a1362b5c73d8235
Carlos Nayan <carlosnayan@gmail.com>
2024-01-22 14:31:40 -0300

    feat: Now it is possible to add more variables to the '.env' file. Validation of data registered in the environment variables file was also added

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import cors from "@fastify/cors";
 import fastify from "fastify";
 import { Routes } from "./routes";
+import dotenv from 'dotenv';
+import { env } from "./env";
 
 const app = fastify();
+
+dotenv.config();
 
 app.register(cors);
 app.register(Routes);
 
-app.listen({ port: 3333, host: "192.168.1.105"}).then(() => {
-  console.log("Rodando em localhost:3333 🚀");
+app.listen({ port: env.PORT, host: env.HOST}).then(() => {
+  console.log(`Rodando em ${env.HOST}:${env.PORT} 🚀`);
 });
